4874099474131619e25671538d0158148b11ab4c
LFrakie <randymas15@hotmail.com>
2023-04-03 14:49:46 -0500

    update

--- iimc.js
+++ iimc.js
@@ -4,8 +4,8 @@ const wasmModule = await WebAssembly.instantiateStreaming(fetch("imc.wasm"), {})
 // Importar la función calcularIMC
 const calcuIMC = wasmModule.instance.exports.calcularIMC;
 
-const peso = 65;
-const altura = 1.64;
+const peso = 60;
+const altura = 1.62;
 
 // Llamar a la función calcularIMC
 const imc = calcuIMC(peso, altura);
